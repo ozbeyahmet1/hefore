1bba21eac5cdcb9d35e84b24cf1b99f59f5451b7
Ahmet Ozbey <ahmet1ozbey@gmail.com>
2023-10-11 02:22:38 +0300

    style: configures tailwind settings

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -7,6 +7,29 @@ const config: Config = {
     "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
+    colors: {
+      pink: "#db00ff",
+      yellow: "#ffa800"
+    },
+    screens: {
+      xs: "450px",
+      // => @media (min-width: 450px) { ... }
+
+      sm: "575px",
+      // => @media (min-width: 576px) { ... }
+
+      md: "768px",
+      // => @media (min-width: 768px) { ... }
+
+      lg: "992px",
+      // => @media (min-width: 992px) { ... }
+
+      xl: "1200px",
+      // => @media (min-width: 1200px) { ... }
+
+      "2xl": "1400px",
+      // => @media (min-width: 1400px) { ... }
+    },
     extend: {
       backgroundImage: {
         "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
