3b07dfbb49d47bc63f686e787dbc6c824fdbb9d6
Ahmet Ozbey <ahmet1ozbey@gmail.com>
2023-11-01 02:29:14 +0300

    feat: creates homepage banner third row

---
+++ src/containers/banner3/index.tsx
@@ -0,0 +1,36 @@
+import Button from "@/components/ui/button";
+import useIntersectionObserver from "@/hooks/useIntersectionObserver";
+import { PropsWithSectionType } from "@/types/homepage";
+import Image from "next/image";
+import { useEffect } from "react";
+
+export default function Banner3({ data: { buttons, image, headline, content } }: PropsWithSectionType) {
+  const [isIntersecting, ref] = useIntersectionObserver("banner3_container", 0.4);
+  useEffect(() => {
+    const targetElement = document.getElementById("banner3_content") as HTMLElement;
+    if (isIntersecting) {
+      targetElement.style.opacity = "1";
+      targetElement.classList.add("slideDown");
+    }
+  }, [isIntersecting]);
+  return (
+    <section className="banner3">
+      <h1 className="banner3_headline">{headline}</h1>
+      <div className="banner3_container" ref={ref}>
+        <div className="banner3--left">
+          <p className="banner3_content" id="banner3_content">
+            {content}
+          </p>
+          {buttons?.map((button, key) => {
+            return (
+              <Button key={key} className={button.variant} as="a" href={button.href}>
+                {button.text}
+              </Button>
+            );
+          })}
+        </div>
+        {image && <Image src={image.src} alt={image.alt} width={660} height={580} className="banner3_image" />}
+      </div>
+    </section>
+  );
+}
