775a82bfa06b3d9fddbd3b8d02cc9befd3265cb6
Ahmet Ozbey <ahmet1ozbey@gmail.com>
2023-11-08 16:14:35 +0300

    feat: scaffolds redux

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,12 +1,15 @@
 import HeadLayout from "@/layouts/head";
 import "@/styles/globals.scss";
 import type { AppProps } from "next/app";
-
+import { Provider } from "react-redux";
+import { store } from "../store/state";
 export default function App({ Component, pageProps }: AppProps) {
   return (
-    <>
-      <HeadLayout />
-      <Component {...pageProps} />
-    </>
+    <Provider store={store}>
+      <>
+        <HeadLayout />
+        <Component {...pageProps} />
+      </>
+    </Provider>
   );
 }
